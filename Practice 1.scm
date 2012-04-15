81bc73b6a90e52cce9536ebb63ff139501d666a8
Carlos Belmonte <carlosbceniza@gmail.com>
2012-04-15 18:38:50 +0200

    Practice 1 completed

--- Practice 1.scm
+++ Practice 1.scm
@@ -33,13 +33,90 @@
 
 (pp (insert-left '(a b c d e f) 'b 'Z))
 
-;;Exercise 4 -  (sust '(a b c a e f a h) 'a 'M) -> '(M b c a e f a h)
+;;Exercise 4 - (sust '(a b c a e f a h) 'a 'M) -> '(M b c a e f a h)
 (define (sust l argl1 argl2)
   (cond
    ((null? l) '())
    ((eq? (car l) argl1)
-    (cons argl2 (sust (cdr l) argl1 argl2)))
+    (cons argl2 (cdr l)))
    (else
     (cons (car l) (sust (cdr l) argl1 argl2)))))
 
-(pp (sust '(a b c d e f g) 'b 'Z))
+(pp (sust '(a b c d b f b) 'b 'Z))
+
+;;Exercise 5 - (element-append '(a b c d e f g h) 'M) -> '(a b c d e f g h M) 
+(define (element-append l argl)
+  (cond
+   ((null? l) '())
+   ((eq? (cdr l) '())
+    (cons argl (element-append (cdr l) argl)))
+   (else
+    (cons (car l) (element-append (cdr l) argl)))))
+
+(pp (element-append '(a b c d e f g) 'H))
+
+;;Exercise 6 - (msust '(a b c a e f a h) 'a 'M) -> '(M b c M e f M h)
+(define (msust l argl1 argl2)
+  (cond 
+   ((null? l) '())
+   ((eq? (car l) argl1)
+    (cons argl2 (msust (cdr l) argl1 argl2)))
+   (else
+    (cons (car l) (msust (cdr l) argl1 argl2)))))
+
+(pp (msust '( a b a c a d a) 'a 'Z))
+
+;;Exercise 7 - (mrember '(a b c a e f a h) 'a) -> '(b c e f h)
+(define (mrember l argl)
+  (cond
+   ((null? l) '())
+   ((eq? (car l) argl)
+    (mrember (cdr l) argl))
+   (else
+    (cons (car l) (mrember (cdr l) argl)))))
+
+(pp (mrember '(a b a c a d a) 'a))
+
+;;Exercise 8 - (nth-element '(a b c d e f g h) 3) -> 'd
+(define (nth-element l argl)
+  (cond
+   ((null? l) '())
+   ((eq? 0 argl)
+    (car l))
+   (else
+    (nth-element (cdr l) (- argl 1)))))
+
+(pp (nth-element '(a b c d e f g) 4))
+
+;;Exercise 9 - (nth-rember '(a b c d e f g h) 3) -> '(a b c e f g h)
+(define (nth-rember l argl)
+  (cond
+   ((null? l) '())
+   ((eq? 0 argl)
+    (cdr l))
+   (else
+    (cons (car l) (nth-rember (cdr l) (- argl 1))))))
+
+(pp (nth-rember '(a b c d e f g) 4))
+
+;;Exercise 10 - (element-occur '(a b c a e f a h) 'a) -> 3
+(define (element-occur l argl)
+  (cond
+   ((null? l) 0)
+   ((eq? (car l) argl)
+    (+ 1 (element-occur (cdr l) argl)))
+   (else
+    (element-occur (cdr l) argl))))
+
+(pp (element-occur '(a b a c a d a) 'a))
+
+;; 11- (length '(a b c d e f g h)) -> 8
+(define (length l)
+  (cond
+   ((null? l) 0)
+   ((eq? (car l) '())
+    (cdr l))
+   (else
+    (+ 1 (length (cdr l))))))
+
+(pp (length '(a b c d e f g)))
