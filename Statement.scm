9ff560ecd022ad345b482698521d71ec0bc01469
Carlos Belmonte <carlosbceniza@gmail.com>
2012-04-27 21:06:07 +0200

    Statement updated

--- Statement.scm
+++ Statement.scm
@@ -1,5 +1,21 @@
 Statements of the Scheme Exercises
 
+;;PRACTICE 1
+
+;; 1-  (list-operation '(a b c d e f g h) 'c) -> '(a b d e f g h)
+;; 2-  (insert-right '(a b c d e f g h) 'c 'M) -> '(a b c M d e f g h)
+;; 3-  (insert-left '(a b c d e f g h) 'c 'M) -> '(a b M c d e f g h)
+;; 4-  (sust '(a b c a e f a h) 'a 'M) -> '(M b c a e f a h)
+;; 5-  (element-append '(a b c d e f g h) 'M) -> '(a b c d e f g h M) 
+;; 6-  (msust '(a b c a e f a h) 'a 'M) -> '(M b c M e f M h)
+;; 7-  (mrember '(a b c a e f a h) 'a) -> '(b c e f h)
+;; 8-  (nth-element '(a b c d e f g h) 3) -> 'd
+;; 9-  (nth-rember '(a b c d e f g h) 3) -> '(a b c e f g h)
+;; 10- (element-occur '(a b c a e f a h) 'a) -> 3
+;; 11- (length '(a b c d e f g h)) -> 8
+
+;;PRACTICE 2 (Practice 1 + Let Recur)
+
 ;; 1-  (list-operation '(a b c d e f g h) 'c) -> '(a b d e f g h)
 ;; 2-  (insert-right '(a b c d e f g h) 'c 'M) -> '(a b c M d e f g h)
 ;; 3-  (insert-left '(a b c d e f g h) 'c 'M) -> '(a b M c d e f g h)
